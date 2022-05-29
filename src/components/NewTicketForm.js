e95190bf05179094bdd5c4cf5f0a05d4fc705055
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-05-29 12:41:04 -0700

    Import prop-types, add props as parameter to NewTicketForm, add proptypes to new prop

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
-function NewTicketForm() {
+function NewTicketForm(props) {
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -21,5 +29,9 @@ function NewTicketForm() {
     </React.Fragment>
   );
 }
+
+NewTicketForm.PropTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;

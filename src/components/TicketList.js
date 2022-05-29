d7fa95f286a73219af6bacd0e6dd27364c878ea4
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-05-29 12:25:28 -0700

    Add props as parameter to TicketList, change map from mainticketlist to props.ticketList, Add propTypes to TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -5,29 +5,11 @@ import { v4 } from 'uuid';
 const ticket = {};
 ticket.id = v4()
 
-const mainTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
-
-function TicketList() {
+function TicketList(props) {
   return (
     <React.Fragment>
       <hr/>
-      {mainTicketList.map((ticket, index) =>
+      {props.ticketList.map((ticket, index) =>
         <Ticket names={ticket.names}
                 location={ticket.location}
                 issue={ticket.issue}
@@ -36,5 +18,9 @@ function TicketList() {
     </React.Fragment>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;

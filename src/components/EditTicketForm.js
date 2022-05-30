76739d1b5c2928c03584804c7e47ee22b78d1574
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-05-29 17:08:24 -0700

    Add edited form submission event handler and proptypes

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
+import PropType from "prop-types";
 
 function EditTicketForm (props) {
+  const { ticket } = props;
+
+  function handleEditTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTicket({names: event.target.names.value,
+                        location: event.target.location.value,
+                        issue: event.target.issue.value,
+                        id: ticket.id});
+  }
+  
   return (
     <React.Fragment>
       <ReusableForm
@@ -9,5 +20,10 @@ function EditTicketForm (props) {
     </React.Fragment>
   );
 }
+
+EditTicketForm.propTypes = {
+  ticket: PropTypes.object,
+  onEditTicket: PropTypes.func
+};
 
 export default EditTicketForm;

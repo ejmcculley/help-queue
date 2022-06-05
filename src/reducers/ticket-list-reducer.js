293482a669a4369f1683ec97120b8de805fd0c6e
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-06-05 15:47:46 -0700

    Add function declaration and export statement

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,4 +1,4 @@
-export default (state = {}, action) => {
+const reducer = (state = {}, action) => {
   const { names, location, issue, id } = action;
   switch (action.type) {
   case "ADD_TICKET":
@@ -18,3 +18,5 @@ export default (state = {}, action) => {
     return state;
   }
 };
+
+export default reducer;

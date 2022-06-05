595be763725fa7db624b598aba5429d2b30dcf8e
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-06-05 12:36:39 -0700

    Add redux and Provider component to index.js with App component as child

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
+import reducer from "./reducers/ticket-list-reducer";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducer);
+
+const root = ReactDOM.render (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
